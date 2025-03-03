696f9e2c5b54697f3b69d0c20a38145cd0003d36
ee <elizabeth.eyers@lumilinks.com>
2025-03-03 22:48:17 +0000

    Update env_setup.R

---
+++ env_setup.R
@@ -0,0 +1,8 @@
+# the tidyverse is a dynamic collection of many essential R packages
+install.packages("tidyverse")
+
+# this tutorial also uses these
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+# to update packages run tidyverse_update()
+
